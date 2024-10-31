008aa4f1b6c0883d17f474c325682762c47eb51e
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2024-10-31 15:25:19 +0530

    Delete src/main/java/works/buddy/samples/vulnerabilities/HardcodedSecretsExample.java

--- src/main/java/works/buddy/samples/vulnerabilities/HardcodedSecretsExample.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class HardcodedSecretsExample {
-    public void printSecret() {
-        String apiKey = "12345-abcde-67890-fghij"; // Hardcoded secret
-        System.out.println("API Key: " + apiKey);
-    }
-}
