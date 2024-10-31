6a5ce69550980f26f4e0c575ffe2c99f13102e50
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2024-10-31 15:25:41 +0530

    Update App.java

--- src/main/java/works/buddy/samples/App.java
+++ src/main/java/works/buddy/samples/App.java
@@ -1,14 +1,10 @@
 package works.buddy.samples;
 
 import works.buddy.samples.vulnerabilities.SQLInjectionExample;
-import works.buddy.samples.vulnerabilities.HardcodedSecretsExample;
 
 public class App {
     public static void main(String[] args) {
         SQLInjectionExample sqlExample = new SQLInjectionExample();
         sqlExample.vulnerableMethod("admin' OR '1'='1"); // Simulate SQL Injection
-
-        HardcodedSecretsExample secretsExample = new HardcodedSecretsExample();
-        secretsExample.printSecret();
     }
 }
