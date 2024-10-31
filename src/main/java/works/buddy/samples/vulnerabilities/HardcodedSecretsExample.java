b9386256e198df5ff8f34108c81108fa3e595f60
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2024-10-31 15:17:45 +0530

    Update and rename HardcodedSecrets.java to HardcodedSecretsExample.java

--- src/main/java/works/buddy/samples/vulnerabilities/HardcodedSecretsExample.java
+++ src/main/java/works/buddy/samples/vulnerabilities/HardcodedSecretsExample.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class HardcodedSecrets {
+public class HardcodedSecretsExample {
     public void printSecret() {
         String apiKey = "12345-abcde-67890-fghij"; // Hardcoded secret
         System.out.println("API Key: " + apiKey);
