f21e24df5dad46334c101b42a97183c0512600b5
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2024-10-31 15:32:07 +0530

    Update App.java

--- src/main/java/works/buddy/samples/App.java
+++ src/main/java/works/buddy/samples/App.java
@@ -2,11 +2,16 @@ package works.buddy.samples;
 
 import works.buddy.samples.vulnerabilities.SQLInjectionExample;
 import works.buddy.samples.vulnerabilities.HardcodedSecretsExample;
+import works.buddy.samples.vulnerabilities.InsecureDeserializationExample;
+
 
 public class App {
     public static void main(String[] args) {
         SQLInjectionExample sqlExample = new SQLInjectionExample();
         sqlExample.vulnerableMethod("admin' OR '1'='1"); // Simulate SQL Injection
+
+        InsecureDeserializationExample deserializationExample = new InsecureDeserializationExample();
+        deserializationExample.vulnerableMethod("someBase64EncodedData");
 
         HardcodedSecretsExample secretsExample = new HardcodedSecretsExample();
         secretsExample.printSecret();
