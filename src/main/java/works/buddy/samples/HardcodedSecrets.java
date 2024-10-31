3fee4d06e02427413458673139e9b2e1bb5486a2
Rohankohli38 <96773549+Rohankohli38@users.noreply.github.com>
2024-10-31 14:45:45 +0530

    Create HardcodedSecrets.java

---
+++ src/main/java/works/buddy/samples/HardcodedSecrets.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class HardcodedSecrets {
+    public void printSecret() {
+        String apiKey = "12345-abcde-67890-fghij"; // Hardcoded secret
+        System.out.println("API Key: " + apiKey);
+    }
+}
